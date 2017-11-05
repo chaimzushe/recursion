3c5ce054df09d41e474303f0ded659ff2e4f95ed
chaim wilmowsky <chaimzushe@gmail.com>
2017-11-05 10:04:09 -0500

    added a more efficient way for exponentiation

--- recusion_algorithm.rb
+++ recusion_algorithm.rb
@@ -6,8 +6,14 @@ def rec_range(start, endd)
   rec_range(start, endd) << endd
 end
 
-
+# a method that return a base number raised to a given power, in a not so efficiant way
 def exp1(base, power)
-   power == 0 ? 1 : base * exp1(base, power -1)
+  power == 0 ? 1 : base * exp1(base, power -1)
+end
 
+# a method that return a base number raised to a given power, in an efficiant way
+def exp2(base, power)
+  return 1 if power == 0
+  half = exp2(base, power / 2)
+  power.even? ? half * half : base * half * half
 end
