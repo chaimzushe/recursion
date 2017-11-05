1076ec640d39ccdfe5020dc328e2aa3e37fd3fc7
chaim wilmowsky <chaimzushe@gmail.com>
2017-11-05 14:42:35 -0500

    Added fibonacci sequence - recursivly

--- recusion_algorithm.rb
+++ recusion_algorithm.rb
@@ -1,17 +1,17 @@
 
-# a method that returns the range from start, til (not including!) endd.
+# A method that returns the range from start, til (not including!) endd.
 def rec_range(start, endd)
   return [] if start >= endd
   endd -= 1
   rec_range(start, endd) << endd
 end
 
-# a method that return a base number raised to a given power, in a not so efficiant way
+# A method that return a base number raised to a given power, in a not so efficiant way
 def exp1(base, power)
   power == 0 ? 1 : base * exp1(base, power -1)
 end
 
-# a method that return a base number raised to a given power, in an efficiant way
+# A method that return a base number raised to a given power, in an efficiant way
 def exp2(base, power)
   return 1 if power == 0
   half = exp2(base, power / 2)
@@ -20,7 +20,6 @@ end
 
 # Ruby keeps things simple by giving you shallow dup, and letting you write deep dup yourself.
 # This method will perform a "deep" duplication of any and all interior arrays.
-
 def deep_dup(arr)
   dupped = []
   arr.each do |el|
@@ -28,7 +27,15 @@ def deep_dup(arr)
   end
   dupped
 end
-# a one liner version of previous method
+
+# A one-line version of the previous method
 def deep_dup(arr)
   arr.map{|el| el.is_a?(Array) ? deep_dup(el) : el}
+end
+
+# A method that returns the an array of numbers in fibonacci order, till the n'th number.
+def fibonacci(n)
+  return [0,1].take(n) if n <= 2
+  fibs = fibonacci(n -1)
+  fibs << fibs[-1] + fibs[-2]
 end
